4f05aeebeb0e12bc477bfe7b08139e183a451b86
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 11:48:36 +0300

    center the texts

--- components/Signals.jsx
+++ components/Signals.jsx
@@ -3,11 +3,9 @@ import React from "react";
 const Signals = () => {
   return (
     <section>
-      <div>
-        <div>
-          <h2>Firebee Signals</h2>
-          <p>A signal for every situation</p>
-        </div>
+      <div className="text-center">
+        <h2>Firebee Signals</h2>
+        <p>A signal for every situation</p>
       </div>
     </section>
   );
