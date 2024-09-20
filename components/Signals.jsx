e55246bf1d276fb34609e11f6161f96df5060031
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 12:39:15 +0300

    mapped over the signals group

--- components/Signals.jsx
+++ components/Signals.jsx
@@ -71,6 +71,14 @@ const Signals = () => {
           A signal for every situation
         </p>
       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {signals.map((signal, idx) => (
+          <div key={idx}>
+            {signal.Image}
+            {signal.description}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
