9229b4c7c4916a153aff6e9909870045dfe594be
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 11:46:29 +0300

    added h2 and p

--- components/Signals.jsx
+++ components/Signals.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Signals = () => {
-  return <div>Signals</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h2>Firebee Signals</h2>
+          <p>A signal for every situation</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signals;
