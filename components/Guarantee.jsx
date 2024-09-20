40be46fffa2f6abdfa9db851fc48a56aa413a174
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 10:02:40 +0300

    styled the p

--- components/Guarantee.jsx
+++ components/Guarantee.jsx
@@ -10,15 +10,15 @@ const Guarantee = () => {
           <Image src={GuaranteeImg} alt="Guarante Img" />
         </div>
         <div className="mt-4">
-          <h2 className=" text-5xl font-light text-[#3F3631]">
+          <h2 className=" text-6xl font-light text-[#3F3631]">
             30 Day Money Back Guarantee
           </h2>
-          <p>
+          <p className=" font-light text-[#3F3631] text-2xl tracking-wide max-w-[790px] mt-10 leading-8">
             At Firebee we believe in building quality products that actually
             work. Firebee and its partners have invested over $85 million in
             research and development to create the highest quality wearable for
             customers to enjoy. If for any reason if you are not satisfied with
-            your Firebee headband we will happily refund your purchase.{" "}
+            your Firebee headband we will happily refund your purchase.
           </p>
         </div>
       </div>
