787c2002c3b6b0893729432945c6ec01e0c945c7
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 11:49:41 +0300

    styled the h2

--- components/Signals.jsx
+++ components/Signals.jsx
@@ -4,7 +4,9 @@ const Signals = () => {
   return (
     <section>
       <div className="text-center">
-        <h2>Firebee Signals</h2>
+        <h2 className="text-5xl font-extralight tracking-wide">
+          Firebee Signals
+        </h2>
         <p>A signal for every situation</p>
       </div>
     </section>
