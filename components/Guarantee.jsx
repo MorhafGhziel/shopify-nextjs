fd4defe7f5285c7c555cbf60c4c0e524f158d7f8
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 10:03:40 +0300

    edited the margin

--- components/Guarantee.jsx
+++ components/Guarantee.jsx
@@ -9,11 +9,11 @@ const Guarantee = () => {
         <div className="flex-shrink-0">
           <Image src={GuaranteeImg} alt="Guarante Img" />
         </div>
-        <div className="mt-4">
+        <div className="mt-6">
           <h2 className=" text-6xl font-light text-[#3F3631]">
             30 Day Money Back Guarantee
           </h2>
-          <p className=" font-light text-[#3F3631] text-2xl tracking-wide max-w-[790px] mt-10 leading-8">
+          <p className=" font-light text-[#3F3631] text-2xl tracking-wide max-w-[790px] mt-8 leading-8">
             At Firebee we believe in building quality products that actually
             work. Firebee and its partners have invested over $85 million in
             research and development to create the highest quality wearable for
