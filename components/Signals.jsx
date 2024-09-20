d5a9f75c51f42fe128f49458324b2ba14d9db91e
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 12:19:57 +0300

    added styles to the p and changed h2 and p color

--- components/Signals.jsx
+++ components/Signals.jsx
@@ -4,10 +4,12 @@ const Signals = () => {
   return (
     <section>
       <div className="text-center">
-        <h2 className="text-5xl font-extralight tracking-wide">
+        <h2 className="text-5xl font-extralight tracking-wide text-[#3F3631]">
           Firebee Signals
         </h2>
-        <p>A signal for every situation</p>
+        <p className="text-[#3F3631] text-xl mt-2 font-light tracking-wide">
+          A signal for every situation
+        </p>
       </div>
     </section>
   );
