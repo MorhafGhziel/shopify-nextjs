417a3077a7c70d2f72189ddb8784fd106ae99750
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 09:34:59 +0300

    added the guarantee img to the page

--- components/Guarantee.jsx
+++ components/Guarantee.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import GuaranteeImg from "@/public/Guarantee.svg";
+import GuaranteeImg from "@/public/GuaranteeImg.svg";
 import Image from "next/image";
 
 const Guarantee = () => {
   return (
     <section>
       <div>
-        <div></div>
+        <div>
+          <Image src={GuaranteeImg} alt="Guarante Img" />
+        </div>
       </div>
     </section>
   );
