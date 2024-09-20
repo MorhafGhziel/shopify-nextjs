460a8a4b66461f270bb7cf420cafc66f9be99b69
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 09:29:17 +0300

    imported guarantee to the page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Guarantee from "@/components/Guarantee";
 
 const page = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Guarantee />
     </>
   );
 };
