b04714a82393f62af287be3bc1672fe35516cc26
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-20 10:28:48 +0300

    the guarantee section is done

--- components/Guarantee.jsx
+++ components/Guarantee.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const Guarantee = () => {
   return (
-    <section className="py-12 px-20">
-      <div className="bg-[#FBFCFB] py-14 px-20 flex gap-12 justify-center flex-wrap md:flex-none">
+    <section className="py-12 md:px-20">
+      <div className="bg-[#FBFCFB] py-14 md:px-20 px-4 flex gap-12 justify-center flex-wrap md:flex-none">
         <div className="flex-shrink-0">
           <Image
             src={GuaranteeImg}
@@ -13,11 +13,11 @@ const Guarantee = () => {
             className="w-[200px] h-[200px] md:h-[292px] md:w-[292px]"
           />
         </div>
-        <div className="mt-6">
-          <h2 className=" text-6xl font-light text-[#3F3631]">
+        <div className="md:mt-6">
+          <h2 className="md:text-5xl text-5xl font-light text-[#3F3631] md:text-left text-center ">
             30 Day Money Back Guarantee
           </h2>
-          <p className=" font-light text-[#3F3631] text-2xl tracking-wide max-w-[790px] mt-8 leading-8">
+          <p className=" font-light text-[#3F3631] text-xl md:text-2xl tracking-wider md:max-w-[790px] mt-8 leading-8 ">
             At Firebee we believe in building quality products that actually
             work. Firebee and its partners have invested over $85 million in
             research and development to create the highest quality wearable for
