5d1caf6ef5ccca2ef4f4cc74b81d2aa189c71bab
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-21 03:53:50 +0300

    styled the h2

--- components/PersonalizedSignals.jsx
+++ components/PersonalizedSignals.jsx
@@ -4,7 +4,9 @@ const PersonalizedSignals = () => {
   return (
     <section className="bg-[#FBFCFB]">
       <div>
-        <h2>What will Firebee do for me?</h2>
+        <h2 className="text-6xl font-extralight tracking-wide text-[#3F3631]">
+          What will Firebee do for me?
+        </h2>
         <p>
           Firebee will help you gently nudge yourself into a different mental
           state
