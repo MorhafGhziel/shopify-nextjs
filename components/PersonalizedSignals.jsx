e8a23cb1a311417aff668de82c2a55d1c9c15579
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-21 15:10:48 +0300

    the section is done at all screens

--- components/PersonalizedSignals.jsx
+++ components/PersonalizedSignals.jsx
@@ -3,7 +3,7 @@ import PersonalizedImg from "@/public/PersonalizedImg.svg";
 import CheckIcon from "@/public/CheckIcon.svg";
 import CheckIconBlack from "@/public/CheckIconBlack.svg";
 import Image from "next/image";
-import DeepTxt from "@/public/DeepTxt.svg";
+import SmallBgSignals from "@/public/SmallBgSignals.svg";
 
 const personalizedSignals = [
   {
@@ -15,7 +15,7 @@ const personalizedSignals = [
       />
     ),
     description: (
-      <div>
+      <div className="ml-[-5px]">
         <span>Can’t sleep? Turn on the </span>
         <span className="bg-gradient-to-r from-[#2E034B] via-[#002182] to-[#2E034B] text-transparent bg-clip-text font-bold">
           deep sleep
@@ -77,28 +77,35 @@ const personalizedSignals = [
 const PersonalizedSignals = () => {
   return (
     <section className="bg-[#FBFCFB] pt-14">
+      <div className="mb-10 justify-center flex">
+        <Image
+          src={SmallBgSignals}
+          alt="Personalized Img"
+          className="md:hidden block"
+        />
+      </div>
       <div className="text-center">
-        <h2 className="text-6xl font-extralight tracking-wide text-[#3F3631]">
+        <h2 className="md:text-6xl text-5xl mx-10 md:mx-0 font-extralight tracking-wide text-[#3F3631]">
           What will Firebee do for me?
         </h2>
-        <p className="text-[#3F3631] text-xl mt-6 font-light tracking-wide">
+        <p className="text-[#3F3631] text-xl mt-6 font-light tracking-wide mx-10 md:mx-0">
           Firebee will help you gently nudge yourself into a different mental
           state
         </p>
       </div>
-      <div className="flex">
-        <div className="md:ml-[200px] ml-0 mx-10">
+      <div className="flex justify-center">
+        <div className=" ml-0 mx-6">
           <Image
-            src={PersonalizedImg}
+            src={SmallBgSignals}
             alt="Personalized Img"
-            className="hidden md:block"
+            className="hidden md:block w-[598px] h-[524px]"
           />
         </div>
-        <div className="mt-16">
+        <div className="md:mt-16 mt-10 md:mx-10 mx-4">
           {personalizedSignals.map((signal, idx) => (
             <div
               key={idx}
-              className="flex items-center md:gap-12 gap-6 mb-8 gap-y-12"
+              className="flex items-center md:gap-10 gap-6 mb-8 gap-y-12"
             >
               {signal.Image}
               <div className="font-extralight tracking-wider text-2xl">
