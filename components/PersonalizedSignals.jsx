31088f7e127cbba3f0a8a2f37a2ae02603f5de59
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-21 03:59:46 +0300

    edited the maring

--- components/PersonalizedSignals.jsx
+++ components/PersonalizedSignals.jsx
@@ -7,7 +7,7 @@ const PersonalizedSignals = () => {
         <h2 className="text-6xl font-extralight tracking-wide text-[#3F3631]">
           What will Firebee do for me?
         </h2>
-        <p className="text-[#3F3631] text-xl mt-2 font-light tracking-wide">
+        <p className="text-[#3F3631] text-xl mt-6 font-light tracking-wide">
           Firebee will help you gently nudge yourself into a different mental
           state
         </p>
