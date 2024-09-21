9b18a2f8a21643000da554a201a419be7a0c339b
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-21 18:19:20 +0300

    added the first linear bg

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="bg-gradient-to-r from-[#8BF2B5] to-[#35C8EB] w-full h-[10px]"></div>
+      <div className="bg-[#12151A]"></div>
+    </footer>
+  );
 };
 
 export default Footer;
